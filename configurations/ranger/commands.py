a569546df0a40df9b92b316fe9d8e533a1dc620a
Nahuel Garbezza <n.garbezza@gmail.com>
2013-09-16 09:16:18 -0300

    add extracthere command to ranger

--- configurations/ranger/commands.py
+++ configurations/ranger/commands.py
@@ -76,7 +76,9 @@
 # of ranger.
 # ===================================================================
 
+import os
 from ranger.api.commands import *
+from ranger.core.loader import CommandLoader
 
 class alias(Command):
     """:alias <newcommand> <oldcommand>
@@ -1242,3 +1244,34 @@ class log(Command):
 
         pager = os.environ.get('PAGER', ranger.DEFAULT_PAGER)
         self.fm.run([pager, tmp.name])
+
+class extracthere(Command):
+    def execute(self):
+        """ Extract copied files to current directory """
+        copied_files = tuple(self.fm.env.copy)
+
+        if not copied_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.env.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = copied_files[0]
+        cwd = self.fm.env.cwd
+        original_path = cwd.path
+        au_flags = ['-X', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.env.copy.clear()
+        self.fm.env.cut = False
+        if len(copied_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags \
+                + [f.path for f in copied_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
