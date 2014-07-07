aa6a17be1af6952308986e14f2f88f3baa0d9ad5
Nahuel Garbezza <n.garbezza@gmail.com>
2014-07-07 16:44:46 -0300

    add repl dependencies to lein profile config file

--- configurations/lein/profiles.clj
+++ configurations/lein/profiles.clj
@@ -1,4 +1,3 @@
-{:user {:plugins [
-  [lein2-eclipse "2.0.0"]
-  [lein-pprint  "1.1.1"]
-]}}
+{:user {:plugins [[lein2-eclipse "2.0.0"]
+                  [lein-pprint  "1.1.1"]]}
+ :repl {:dependencies [[thalia "0.1.0"]]}}
