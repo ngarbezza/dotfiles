24de64c1200ccd59e9a521bad7864195564f053e
Nahuel Garbezza <n.garbezza@gmail.com>
2016-07-03 12:02:36 -0300

    rename to .sh - aliases

---
+++ bash/aliases/base.sh
@@ -0,0 +1,17 @@
+ALIASES_PATH=${BASE_PATH}/aliases
+
+source ${ALIASES_PATH}/apps.sh
+source ${ALIASES_PATH}/arch.sh
+source ${ALIASES_PATH}/clojure.sh
+source ${ALIASES_PATH}/code.sh
+source ${ALIASES_PATH}/git.sh
+source ${ALIASES_PATH}/heroku.sh
+source ${ALIASES_PATH}/java.sh
+source ${ALIASES_PATH}/js.sh
+source ${ALIASES_PATH}/mercurial.sh
+source ${ALIASES_PATH}/other.sh
+source ${ALIASES_PATH}/pacman.sh
+source ${ALIASES_PATH}/ruby.sh
+source ${ALIASES_PATH}/svn.sh
+source ${ALIASES_PATH}/unix.sh
+source ${ALIASES_PATH}/vagrant.sh
