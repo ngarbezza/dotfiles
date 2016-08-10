01506abb0cb696d56a25803653cf22da822e06e6
Nahuel Garbezza <n.garbezza@gmail.com>
2016-08-10 09:55:39 -0300

    rename .sh prompt

--- bash/prompt/base.sh
+++ bash/prompt/base.sh
@@ -1,6 +1,6 @@
 PROMPT_PATH=$BASE_PATH/prompt
 
-source $PROMPT_PATH/colors
+source $PROMPT_PATH/colors.sh
 source $PROMPT_PATH/git-prompt.sh
-source $PROMPT_PATH/hg-prompt
-source $PROMPT_PATH/ps1
+source $PROMPT_PATH/hg-prompt.sh
+source $PROMPT_PATH/ps1.sh
