095477796f5e520b621d10ab1f444b52e3595faa
Nahuel Garbezza <n.garbezza@gmail.com>
2016-08-10 09:54:18 -0300

    rename .sh completions and functions

---
+++ bash/completions/base.sh
@@ -0,0 +1,7 @@
+COMPLETIONS_PATH=${BASE_PATH}/completions
+
+source ${COMPLETIONS_PATH}/make_completion_wrapper.sh
+source ${COMPLETIONS_PATH}/git.sh
+complete -C ${COMPLETIONS_PATH}/pharo.sh -o default pharo
+source ${COMPLETIONS_PATH}/rake.sh
+source ${COMPLETIONS_PATH}/lein.sh
---
+++ bash/functions/base.sh
@@ -0,0 +1,8 @@
+FUNCTIONS_PATH=$BASE_PATH/functions
+
+source $FUNCTIONS_PATH/cb.sh
+source $FUNCTIONS_PATH/git.sh
+source $FUNCTIONS_PATH/bash.sh
+source $FUNCTIONS_PATH/extract.sh
+source $FUNCTIONS_PATH/octopress.sh
+source $FUNCTIONS_PATH/ruby.sh
