3b31fa063f6bd2d713b7b06522284170851fa9a0
Nahuel Garbezza <n.garbezza@gmail.com>
2016-09-14 12:00:15 -0300

    add install script for nvm on arch linux

--- bash/aliases/js.sh
+++ bash/aliases/js.sh
@@ -3,3 +3,6 @@ alias jsi='npm install && bower install'
 # ember
 alias es='ember server'
 alias eg='ember generate'
+
+# nvm
+alias nvmi='cat .nvmrc | nvm install'
---
+++ install-scripts/arch-linux/nvm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+yaourt -S --noconfirm nvm
+echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
